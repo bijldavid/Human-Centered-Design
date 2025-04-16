c12c4c0c3ead447b084b0155130312aa258a76db
David Bijl <davidbijl321@gmail.com>
2025-04-16 13:29:13 +0200

    update

---
+++ captions.js
@@ -0,0 +1,48 @@
+
+captions[0] = ['0', '1.55'];
+captions[1] = ['4.5', '6'];
+captions[2] = ['5', '6'];
+captions[3] = ['6.2', '8.5'];
+captions[4] = ['8', '10.5'];
+captions[5] = ['10', '13'];
+
+// That's some bad hat harry
+captions[6] = ['12.5', '14.8'];
+
+// Harry puffs
+captions[7] = ['14', '16'];
+
+// Splash noises
+captions[8] = ['16', '20'];
+captions[9] = ['20', '23'];
+captions[10] = ['20', '23'];
+
+// Child singing
+captions[11] = ['23', '28'];
+captions[12] = ['23', '26'];
+captions[13] = ['26', '28'];
+captions[14] = ['28', '32'];
+
+// Guy yelling
+captions[15] = ['28', '30.5'];
+captions[16] = ['30', '32.5'];
+captions[17] = ['32.5', '36'];
+captions[18] = ['35.5', '37.5'];
+
+// Shark
+captions[19] = ['38', '50'];
+captions[20] = ['42', '50'];
+captions[21] = ['50', '55'];
+
+captions[22] = ['59.7', '62'];
+captions[23] = ['61', '62.5'];
+
+captions[24] = ['62', '66'];
+
+// child yelling
+captions[25] = ['63', '65'];
+captions[26] = ['64', '66'];
+captions[27] = ['66', '67.5'];
+
+// music intensifies
+captions[28] = ['67.5', '70'];
---
+++ yt.js
@@ -0,0 +1,180 @@
+var player;
+
+function onYouTubeIframeAPIReady() {
+    player = new YT.Player('video-placeholder', {
+        videoId: 'QSU5WtVFDVw?si=HDH1uYJIRlUoLRgW',
+        playerVars: {
+            modestbranding: 1,     // Hide YouTube logo
+            rel: 0,                // Don't show related videos
+            showinfo: 0,           // Hide title (deprecated but still works sometimes)
+            fs: 0,                 // Disable fullscreen button
+            autoplay: 1,           // Autoplay if you want
+            playsinline: 1,        // Prevent fullscreen on mobile
+        },
+        events: {
+            onReady: initialize
+        }
+    });
+
+}
+
+function initialize() {
+    // Update the controls on load
+    addSpans();
+}
+function addSpans() {
+    var ps = document.querySelectorAll('#closed-captions p');
+    var i = 0;
+    var regex = /\S+/g;
+    while (i < ps.length) {
+        var str = ps[i].innerText;
+        var result = str.replace(regex, function (a) {
+            return "<span>" + a + "</span>";
+        });
+        ps[i].innerHTML = result;
+        ps[i].classList.add('p' + i);
+        i++;
+    }
+    updateTimerDisplay();
+}
+
+function updateTimerDisplay() {
+    var t = player.getCurrentTime();
+    t = Math.floor10(t, -1);
+
+    // Process captions
+    var i = 0;
+    while (i < captions.length) {
+        pTimes(i, captions[i][0], captions[i][1], t);
+        i++;
+    }
+
+    // Process effects
+    var i = 0;
+    while (i < effects.length) {
+        eTimes(i, effects[i], t);
+        i++;
+    }
+
+    // Process background changes
+    var i = 0;
+    while (i < backgrounds.length) {
+        bTimes(i, backgrounds[i], t);
+        i++;
+    }
+
+    // Change 136.1 to the length of your own video in seconds
+    if (t < 136.1) {
+        setTimeout(() => {
+            updateTimerDisplay();
+        }, 100);
+    }
+}
+
+function pTimes(num, startT, endT, curT) {
+    var curP = document.querySelector('.p' + num);
+    if (curT > endT && !curP.classList.contains('off')) {
+        curP.classList.add('off');
+    }
+    if (curT < endT && curP.classList.contains('off')) {
+        curP.classList.remove('off');
+    }
+    if (curT > startT && !curP.classList.contains('on')) {
+        curP.classList.add('on');
+    }
+    if (curT < startT && curP.classList.contains('on')) {
+        curP.classList.remove('on');
+    }
+}
+
+function eTimes(num, effectTiming, curT) {
+    var effectClass = 'effect' + num;
+    var b = document.querySelector('.video-effects');
+
+    // Extract start and end times
+    var startTime = effectTiming[0];
+    var endTime = effectTiming[1];
+
+    // Add class when current time is between start and end times
+    if (curT >= startTime && curT <= endTime && !b.classList.contains(effectClass)) {
+        b.classList.add(effectClass);
+    }
+
+    // Remove class when current time is outside the range
+    if ((curT < startTime || curT > endTime) && b.classList.contains(effectClass)) {
+        b.classList.remove(effectClass);
+    }
+}
+
+function bTimes(num, backgroundTiming, curT) {
+    var backgroundClass = 'background' + num;
+    var body = document.getElementById('video-background');
+
+    // Extract start and end times
+    var startTime = backgroundTiming[0];
+    var endTime = backgroundTiming[1];
+
+    // Add class when current time is between start and end times
+    if (curT >= startTime && curT <= endTime && !body.classList.contains(backgroundClass)) {
+        body.classList.add(backgroundClass);
+    }
+
+    // Remove class when current time is outside the range
+    if ((curT < startTime || curT > endTime) && body.classList.contains(backgroundClass)) {
+        body.classList.remove(backgroundClass);
+    }
+}
+
+(function () {
+    /**
+     * Decimal adjustment of a number.
+     *
+     * @param {String}  type  The type of adjustment.
+     * @param {Number}  value The number.
+     * @param {Integer} exp   The exponent (the 10 logarithm of the adjustment base).
+     * @returns {Number} The adjusted value.
+     */
+    function decimalAdjust(type, value, exp) {
+        // If the exp is undefined or zero...
+        if (typeof exp === 'undefined' || +exp === 0) {
+            return Math[type](value);
+        }
+        value = +value;
+        exp = +exp;
+        // If the value is not a number or the exp is not an integer...
+        if (isNaN(value) || !(typeof exp === 'number' && exp % 1 === 0)) {
+            return NaN;
+        }
+        // Shift
+        value = value.toString().split('e');
+        value = Math[type](+(value[0] + 'e' + (value[1] ? (+value[1] - exp) : -exp)));
+        // Shift back
+        value = value.toString().split('e');
+        return +(value[0] + 'e' + (value[1] ? (+value[1] + exp) : exp));
+    }
+
+    // Decimal round
+    if (!Math.round10) {
+        Math.round10 = function (value, exp) {
+            return decimalAdjust('round', value, exp);
+        };
+    }
+    // Decimal floor
+    if (!Math.floor10) {
+        Math.floor10 = function (value, exp) {
+            return decimalAdjust('floor', value, exp);
+        };
+    }
+    // Decimal ceil
+    if (!Math.ceil10) {
+        Math.ceil10 = function (value, exp) {
+            return decimalAdjust('ceil', value, exp);
+        };
+    }
+})();
+
+
+document.getElementById('overlay').addEventListener('click', () => {
+    player.playVideo();
+    document.getElementById('overlay').style.display = 'none';
+});
